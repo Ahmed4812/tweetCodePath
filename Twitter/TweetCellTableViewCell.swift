c484ac6104bcb3de3b2a636b54fbef771c4085a4
Ali Ahmed Khan <aliahmedkhan4812@gmail.com>
2022-03-21 01:12:07 -0400

    finished u4 basic

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -14,6 +14,39 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
     
+    @IBOutlet weak var favButton: UIButton!
+    
+    @IBAction func onFavTweet(_ sender: Any) {
+        let toBeFav = !favorite
+        if (toBeFav){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: {
+                (error) in
+                print("fav didn't work \(error)")
+            })
+        }else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: {
+                (error) in
+                print("fav didn't work \(error)")
+            })
+            
+        }
+    }
+    var favorite:Bool = false
+    var tweetId: Int = -1
+    func setFavorite(_ isfavorite:Bool){
+        favorite = isfavorite
+        if (favorite){
+            favButton.setImage(UIImage(named: "favor-icon-1"), for: UIControl.State.normal)
+        }else{
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
